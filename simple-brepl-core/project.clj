479fa44a6afd473329b08aefc0d493e10815baa4
James Henderson <james@jarohen.me.uk>
2014-05-10 12:11:59 +0100

    Abstracting out the project version

--- simple-brepl-core/project.clj
+++ simple-brepl-core/project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/simple-brepl-core "0.1.0-SNAPSHOT"
+(defproject jarohen/simple-brepl-core (slurp (clojure.java.io/file "../resources/simple_brepl/VERSION"))
   :description "Functionality for simple-brepl plugin to call"
   :url "https://github.com/james-henderson/simple-brepl"
   :license {:name "Eclipse Public License"
@@ -7,4 +7,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [weasel "0.2.0"]
                  [com.cemerick/piggieback "0.1.3"]
-                 [org.clojure/clojurescript "0.0-2202"]])
+                 [org.clojure/clojurescript "0.0-2202"]]
+
+  :scm {:dir ".."}
+
+  :resource-paths ["resources" "../resources"])
--- simple-brepl/project.clj
+++ simple-brepl/project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/simple-brepl "0.1.0-SNAPSHOT"
+(defproject jarohen/simple-brepl (slurp (clojure.java.io/file "../resources/simple_brepl/VERSION"))
   :description "A plugin to make opening a CLJS browser REPL simple, based atop Weasel"
   :url "https://github.com/james-henderson/simple-brepl"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,8 @@
 
   :dependencies [[leinjacker "0.4.2"]]
   
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :scm {:dir ".."}
+
+  :resource-paths ["resources" "../resources"])
