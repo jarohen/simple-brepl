e83c2f4ecdf5c9c2e1dc095dcf2c3f710fd73e48
James Henderson <james@jarohen.me.uk>
2014-05-10 11:38:49 +0100

    Initial commit

---
+++ simple-brepl-core/project.clj
@@ -0,0 +1,8 @@
+(defproject jarohen/simple-brepl-core "0.1.0-SNAPSHOT"
+  :description "Functionality for simple-brepl plugin to call"
+  :url "https://github.com/james-henderson/simple-brepl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [weasel "0.2.0"]])
---
+++ simple-brepl/project.clj
@@ -0,0 +1,9 @@
+(defproject jarohen/simple-brepl "0.1.0-SNAPSHOT"
+  :description "A plugin to make opening a CLJS browser REPL simple, based atop Weasel"
+  :url "https://github.com/james-henderson/simple-brepl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[leinjacker "0.4.2"]]
+  
+  :eval-in-leiningen true)
