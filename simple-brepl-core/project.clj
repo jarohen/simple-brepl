624cea3986fc766faedf1a35b33222ef16251511
James Henderson <james@jarohen.me.uk>
2014-05-10 11:55:25 +0100

    [core] Adding piggieback and CLJS deps

--- simple-brepl-core/project.clj
+++ simple-brepl-core/project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [weasel "0.2.0"]])
+                 [weasel "0.2.0"]
+                 [com.cemerick/piggieback "0.1.3"]
+                 [org.clojure/clojurescript "0.0-2202"]])
