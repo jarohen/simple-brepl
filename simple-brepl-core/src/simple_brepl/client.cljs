5ba3bd42b58289fbdcaf00023e3b312b5a265805
Joel Friedman <asher.friedman@gmail.com>
2014-11-10 16:29:55 -0600

    safer read of vars off of js/window
    * don't throw exception if not present

--- simple-brepl-core/src/simple_brepl/client.cljs
+++ simple-brepl-core/src/simple_brepl/client.cljs
@@ -1,7 +1,7 @@
 (ns simple-brepl.client
   (:require [weasel.repl :as ws-repl]))
 
-(when-let [repl-ip js/simple_brepl_ip]
-  (when-let [repl-port js/simple_brepl_port]
+(when-let [repl-ip (.-simple_brepl_ip js/window)]
+  (when-let [repl-port (.-simple_brepl_port js/window)]
     (ws-repl/connect (str "ws://" repl-ip ":" repl-port))))
 
