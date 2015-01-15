01169c5f650e768f8f65df238d452afa099537f7
James Henderson <james@jarohen.me.uk>
2015-01-15 17:47:20 +0000

    Updating Piggieback to 0.1.5, thanks @mfikes

--- simple-brepl-core/project.clj
+++ simple-brepl-core/project.clj
@@ -6,7 +6,7 @@
   
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [weasel "0.5.0"]
-                 [com.cemerick/piggieback "0.1.4"]
+                 [com.cemerick/piggieback "0.1.5"]
                  [org.clojure/clojurescript "0.0-2665"]]
 
   :scm {:dir ".."}
